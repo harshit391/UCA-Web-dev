d45d848652b5c6c94f6af4a62532b76400f60615
Harshit Singla <harshitsingla1761@gmail.com>
2024-11-13 20:34:36 +0530

    BackEnd CRUD Completed : CURD Through MongoDB

--- React/class-project/Backend/models/product.model.js
+++ React/class-project/Backend/models/product.model.js
@@ -1,17 +1,104 @@
 import mongoose from "mongoose";
 
-const productSchema = new mongoose.Schema(
+/* ----------------------------------------------------------------------------------------------------------- */
+/* Defining The Schema */
+const productsSchema = new mongoose.Schema(
 {
-    name: {
+    productName: {
         type: String,
         required: true
     },
-    price: {
+    productPrice: {
         type: Number,
         required: true
     }
 });
 
-const Product = mongoose.model("Product", productSchema);
+/* Creating The Model of Schema
+    It Creates a reference to Call That Scheme as a Function to create a Collection */
+const ProductsModel = mongoose.model("product", productsSchema);
 
-export default Product;
+/* ----------------------------------------------------------------------------------------------------------- */
+ProductsModel.findAllProducts = async (sucessCallBack, errorCallback) => 
+{
+    try
+    {
+        const dbRes = await ProductsModel.find({});
+        console.log("Get | dbres is: ", dbRes);
+        sucessCallBack(dbRes);
+    }
+    catch (error)
+    {
+        console.error("Get | dbErr is: ", error);
+        errorCallback(error);
+    }
+};
+
+ProductsModel.findProduct = async (productId, sucessCallBack, errorCallback) =>
+{
+    try
+    {
+        const dbRes = await ProductsModel.findById(productId);
+        console.log("Get | dbres is: ", dbRes);
+        sucessCallBack(dbRes);
+    }
+    catch (error)
+    {
+        console.error("Get | dbErr is: ", error);
+        errorCallback(error);
+    }
+}
+
+/* ----------------------------------------------------------------------------------------------------------- */
+/* CRUD Operations */
+
+// 1. Create a new product
+ProductsModel.addProduct = async (product, sucessCallBack, errorCallback) => 
+{
+    try 
+    {
+        const dbRes = await ProductsModel.insertMany([product]);
+        console.log("Post | dbres is: ", dbRes);
+        sucessCallBack(dbRes);
+    } 
+    catch (error) 
+    {
+        console.error("Post | dbErr is: ", error);
+        errorCallback(error);
+    }
+};
+
+// 2. Update an existing product
+ProductsModel.editProduct = async (product, sucessCallBack, errorCallback) => 
+{
+    try 
+    {
+        const dbRes = await ProductsModel.findByIdAndUpdate(product._id, product, {new: true});
+        console.log("Put | dbres is: ", dbRes);
+        sucessCallBack(dbRes);
+    } 
+    catch (error) 
+    {
+        console.error("Put | dbErr is: ", error);
+        errorCallback(error);
+    }
+};
+
+// 3. Delete an existing product
+ProductsModel.deleteProduct = async (productId, sucessCallBack, errorCallback) =>
+{
+    try 
+    {
+        const dbRes = await ProductsModel.findByIdAndDelete(productId);
+        console.log("Delete | dbres is: ", dbRes);
+        sucessCallBack(dbRes);
+    } 
+    catch (error) 
+    {
+        console.error("Delete | dbErr is: ", error);
+        errorCallback(error);
+    }
+}
+
+export default ProductsModel;
+
--- React/class-project/Backend/routes/products.routes.js
+++ React/class-project/Backend/routes/products.routes.js
@@ -1,5 +1,6 @@
 import express from "express";
-import fs from "fs";
+// import fs from "fs";
+import ProductsModel from "../models/product.model.js";
 
 // You can also do this
 // export const router = express.Router();
@@ -9,152 +10,272 @@ import fs from "fs";
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    const data = fs.readFileSync("db.json");
+/* --------------------------- GET REQUEST --------------------------- */
+// router.get("/", (req, res) => {
+//     const data = fs.readFileSync("db.json");
 
-    const products = JSON.parse(data)?.products;
+//     const products = JSON.parse(data)?.products;
 
-    if (products && products.length > 0)
-    {
-        res.status(200).send(products);
-    }
-    else
-    {   
-        res.status(204).send("No Products Found");
-    }
+//     if (products && products.length > 0)
+//     {
+//         res.status(200).send(products);
+//     }
+//     else
+//     {   
+//         res.status(204).send("No Products Found");
+//     }
+// })
+
+router.get("/", (req, res) => 
+{
+    ProductsModel.findAllProducts(
+
+        // Success Callback
+        (dbres) => 
+        {
+            if (dbres && dbres.length > 0)
+            res.status(200).send({message: "Request Successfull", product: dbres});
+
+            else
+            res.status(204).send({message: "No Products Found", product: dbres});  
+        }, 
+
+        // Error Callback
+        (dbres ) =>
+        {
+            res.status(500).send({message : "Product Creation Failed", error: dbres});
+        }
+    );   
 })
+
+router.get("/:id", (req, res) =>
+{
+    const productId = req.params.id;
+
+    ProductsModel.findProduct(productId,
+
+        // Success Callback
+        (dbres) => 
+        {
+            if (dbres && dbres.length > 0)
+            res.status(201).send({message: "Product Created Successfully", product: dbres});
+
+            else
+            res.status(204).send({message: "No Products Found", product: dbres});
+        }, 
+
+        // Error Callback
+        (dbres ) =>
+        {
+            res.status(500).send({message : "Product Creation Failed", error: dbres});
+        }
+    );
+});
+
+/* --------------------------- POST REQUEST --------------------------- */
+// router.post("/", (req, res) => {
+    
+//     const product = req.body;
+
+//     console.log("The Product Object :- ", product);
+
+//     let db = fs.readFileSync("db.json");
+
+//     try 
+//     {
+//         db = fs.readFileSync("db.json", {encoding: "utf-8"});
+//     }
+//     catch (error) 
+//     {
+//         return res.status(500).send({message: "Data Read Error", error: error});
+//     }
+
+//     const dbObject = JSON.parse(db);
+
+//     const products = dbObject.products;
+
+//     const newProducts = [...products, product];
+
+//     const updatedDb = {
+//         ...dbObject,
+
+//         products: newProducts
+//     }
+
+//     try 
+//     {       
+//         fs.writeFileSync("db.json", JSON.stringify(updatedDb));
+//         res.status(201).send({message: "Product Created Successfully", product: product});  
+//     } catch (error) 
+//     {
+//         res.status(500).send({message : "Product Creation Failed", error: error});
+//     } 
+// })
 
 router.post("/", (req, res) => {
-    
     const product = req.body;
+    
+    ProductsModel.addProduct(product,
 
-    console.log("The Product Object :- ", product);
+        // Success Callback
+        (dbres) => 
+        {
+            res.status(201).send({message: "Product Creation Successfully", product: dbres});   
+        }, 
 
-    let db = fs.readFileSync("db.json");
+        // Error Callback
+        (dbres ) =>
+        {  
+            if (dbres.Error == 'ValidationError') 
+            res.status(400).send({message : "Invalid Details from Client", error: dbres});
 
-    try 
-    {
-        db = fs.readFileSync("db.json", {encoding: "utf-8"});
-    }
-    catch (error) 
-    {
-        return res.status(500).send({message: "Data Read Error", error: error});
-    }
-
-    const dbObject = JSON.parse(db);
-
-    const products = dbObject.products;
-
-    const newProducts = [...products, product];
-
-    const updatedDb = {
-        ...dbObject,
-
-        products: newProducts
-    }
-
-    try 
-    {       
-        fs.writeFileSync("db.json", JSON.stringify(updatedDb));
-        res.status(201).send({message: "Product Created Successfully", product: product});  
-    } catch (error) 
-    {
-        res.status(500).send({message : "Product Creation Failed", error: error});
-    } 
+            else
+            res.status(500).send({message : "Product Updation Failed", error: dbres});
+        }
+    );
 })
+
+/* --------------------------- PUT REQUEST --------------------------- */
+// router.put("/", (req, res) => {
+    
+//     const product = req.body;
+
+//     console.log("The Product Object :- ", product);
+
+//     let db = fs.readFileSync("db.json");
+
+//     try 
+//     {
+//         db = fs.readFileSync("db.json", {encoding: "utf-8"});
+//     }
+//     catch (error) 
+//     {
+//         return res.status(500).send({message: "Data Read Error", error: error});
+//     }
+
+//     const dbObject = JSON.parse(db);
+
+//     const products = dbObject.products;
+
+//     const productToBeUpdatedIdx = products.findIndex(p => p.id === product.id);
+
+//     if (productToBeUpdatedIdx === -1)
+//     {
+//         return res.status(404).send({message: "Product Not Found"});
+//     }
+
+//     const newProducts = [...products];
+
+//     newProducts[productToBeUpdatedIdx] = product;
+
+//     const updatedDb = {
+//         ...dbObject,
+
+//         products: newProducts
+//     }
+
+//     try 
+//     {       
+//         fs.writeFileSync("db.json", JSON.stringify(updatedDb));
+//         res.status(201).send({message: "Product Updated Successfully", product: product});  
+//     } catch (error) 
+//     {
+//         res.status(500).send({message : "Product Updation Failed", error: error});
+//     } 
+// })
 
 router.put("/", (req, res) => {
-    
     const product = req.body;
+    
+    ProductsModel.editProduct(product,
 
-    console.log("The Product Object :- ", product);
+        // Success Callback
+        (dbres) => 
+        {
+            res.status(201).send({message: "Product Updated Successfully", product: dbres});            
+        }, 
 
-    let db = fs.readFileSync("db.json");
+        // Error Callback
+        (dbres ) =>
+        {  
+            if (dbres.Error == 'ValidationError') 
+            res.status(400).send({message : "Invalid Details from Client", error: dbres});
 
-    try 
-    {
-        db = fs.readFileSync("db.json", {encoding: "utf-8"});
-    }
-    catch (error) 
-    {
-        return res.status(500).send({message: "Data Read Error", error: error});
-    }
-
-    const dbObject = JSON.parse(db);
-
-    const products = dbObject.products;
-
-    const productToBeUpdatedIdx = products.findIndex(p => p.id === product.id);
-
-    if (productToBeUpdatedIdx === -1)
-    {
-        return res.status(404).send({message: "Product Not Found"});
-    }
-
-    const newProducts = [...products];
-
-    newProducts[productToBeUpdatedIdx] = product;
-
-    const updatedDb = {
-        ...dbObject,
-
-        products: newProducts
-    }
-
-    try 
-    {       
-        fs.writeFileSync("db.json", JSON.stringify(updatedDb));
-        res.status(201).send({message: "Product Updated Successfully", product: product});  
-    } catch (error) 
-    {
-        res.status(500).send({message : "Product Updation Failed", error: error});
-    } 
+            else
+            res.status(500).send({message : "Product Updation Failed", error: dbres});
+        }
+    );
 })
 
-router.delete("/", (req, res) => {
+
+/* --------------------------- DELETE REQUEST --------------------------- */
+// router.delete("/", (req, res) => {
         
-        const productId = req.body.id;
+//         const productId = req.body.id;
     
-        console.log("The Product Id :- ", productId);
+//         console.log("The Product Id :- ", productId);
     
-        let db = fs.readFileSync("db.json");
+//         let db = fs.readFileSync("db.json");
     
-        try 
+//         try 
+//         {
+//             db = fs.readFileSync("db.json", {encoding: "utf-8"});
+//         }
+//         catch (error) 
+//         {
+//             return res.status(500).send({message: "Data Read Error", error: error});
+//         }
+    
+//         const dbObject = JSON.parse(db);
+    
+//         const products = dbObject.products;
+    
+//         const productToBeDeletedIdx = products.findIndex(p => p.id === productId);
+    
+//         if (productToBeDeletedIdx === -1)
+//         {
+//             return res.status(404).send({message: "Product Not Found"});
+//         }
+    
+//         const newProducts = products.filter(p => p.id !== productId);
+    
+//         const updatedDb = {
+//             ...dbObject,
+    
+//             products: newProducts
+//         }
+    
+//         try 
+//         {       
+//             fs.writeFileSync("db.json", JSON.stringify(updatedDb));
+//             res.status(201).send({message: "Product Deleted Successfully", productId: productId});  
+//         } catch (error) 
+//         {
+//             res.status(500).send({message : "Product Deletion Failed", error: error});
+//         } 
+// });
+
+router.delete("/", (req, res) => { 
+    const productId = req.body._id;
+    
+    ProductsModel.deleteProduct(productId,
+
+        // Success Callback
+        (dbres) => 
         {
-            db = fs.readFileSync("db.json", {encoding: "utf-8"});
-        }
-        catch (error) 
+            res.status(201).send({message: "Product Deleted Successfully", productId: dbres});  
+        }, 
+
+        // Error Callback
+        (dbres ) =>
         {
-            return res.status(500).send({message: "Data Read Error", error: error});
+            if (dbres.Error == 'ValidationError')
+            res.status(400).send({message : "Invalid Details from Client", error: dbres});
+
+            else
+            res.status(500).send({message : "Product Deletion Failed", error: dbres});
         }
-    
-        const dbObject = JSON.parse(db);
-    
-        const products = dbObject.products;
-    
-        const productToBeDeletedIdx = products.findIndex(p => p.id === productId);
-    
-        if (productToBeDeletedIdx === -1)
-        {
-            return res.status(404).send({message: "Product Not Found"});
-        }
-    
-        const newProducts = products.filter(p => p.id !== productId);
-    
-        const updatedDb = {
-            ...dbObject,
-    
-            products: newProducts
-        }
-    
-        try 
-        {       
-            fs.writeFileSync("db.json", JSON.stringify(updatedDb));
-            res.status(201).send({message: "Product Deleted Successfully", productId: productId});  
-        } catch (error) 
-        {
-            res.status(500).send({message : "Product Deletion Failed", error: error});
-        } 
+    );
 });
 
 export default router;
